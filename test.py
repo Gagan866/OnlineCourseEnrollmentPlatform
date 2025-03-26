a1b4e13642a30245b0562cc6f5686319caca658e
Gagan866 <gd0363264@gmail.com>
2025-03-26 12:24:37 +0530

    updated file

--- test.py
+++ test.py
@@ -2,5 +2,4 @@ from course_enroll import enroll_student
 
 def test():
     assert enroll_student("Rahul","Python") == "Student added to python"
-    assert enroll_student("Rahul","Java") == "Student added to Java"
-    assert enroll_student("Rahul","C++") == "Invalid course"
+   
